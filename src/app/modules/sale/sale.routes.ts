6dddfea1b1f8a14624e11409b2c5a3ac4304249e
Supravat Sarkar <supravat.sarkar@oyelabs.com>
2024-04-28 19:39:26 +0530

    add .env.example , protect sales route, add build script

--- src/app/modules/sale/sale.routes.ts
+++ src/app/modules/sale/sale.routes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { saleValidation } from "./sale.validation";
 import validateRequest from "../../middlewares/validateRequest";
 import { saleController } from "./sale.controller";
+import auth from "../../middlewares/auth";
 const router = Router();
 router.post(
   "/mark-as-sale/:productId",
+  auth("admin", "manager"),
   validateRequest(saleValidation.markAsSell),
   saleController.markAsSell
 );
